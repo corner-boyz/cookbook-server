36131623ef8898f0ac78304d0c0cca19838f29e8
Joseph Oh <ohyjoseph@gmail.com>
2018-06-25 19:56:39 -0500

    Renamed recipes endpoint to recipelist

--- server/server.js
+++ server/server.js
@@ -70,7 +70,7 @@ app.post('/api/parse', (req, res) => {
   res.send(parsed);
 });
 
-app.post('/api/recipes', (req, res) => {
+app.post('/api/recipelist', (req, res) => {
   //temporarily here to test server and client
   const testRecipes = require('./testRecipes.json');
   res.send(testRecipes);
