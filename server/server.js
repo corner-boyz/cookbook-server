d519b29f5de13fdd7634a54629d7933b0c8d9c21
Joseph Oh <ohyjoseph@gmail.com>
2018-07-02 20:11:26 -0500

    Added helper functions on server to helpers file

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const dbHelpers = require('../database/dbHelpers');
+const helpers = require('./helpers');
 const extCalls = require('./extCalls');
 
 const app = express();
 
 const { parse, combine } = require('recipe-ingredient-parser');
 const convert = require('convert-units');
-const pluralize = require('pluralize');
 
 const bcrypt = require('bcrypt');
 
@@ -90,7 +90,7 @@ app.post('/api/ingredients', (req, res) => {
   const { email, ingredients, shouldReplace } = req.body;
   const table = 'ingredients';
   ingredients.forEach(object => {
-    object.ingredient = pluralize.singular(object.ingredient);
+    object.ingredient = helpers.pluralize.singular(object.ingredient);
   });
   Promise.all(dbHelpers.insertIngredients({ email: email, ingredients: ingredients, shouldReplace: shouldReplace, table: table }))
     .then((results) => {
@@ -106,14 +106,14 @@ app.post('/api/ingredients', (req, res) => {
     });
 });
 
-app.post('/api/grocerylist', (req, res) => {
+app.post('/api/grocerylist', async (req, res) => {
   const { email, ingredients, shouldReplace} = req.body;
   // console.log('PURCHASE', ingredients[0].ispurchased)
   const table = 'grocerylist';
   ingredients.forEach(object => {
-    object.ingredient = pluralize.singular(object.ingredient);
+    object.ingredient = helpers.pluralize.singular(object.ingredient);
   });
-  Promise.all(dbHelpers.insertIngredients({ email: email, ingredients: ingredients, shouldReplace: shouldReplace, table: table }))
+  await Promise.all(dbHelpers.insertIngredients({ email: email, ingredients: ingredients, shouldReplace: shouldReplace, table: table }))
     .then((results) => {
       console.log('SUCCESS inserting into groceryList', results);
       return dbHelpers.groceryListIntoIngredients({email: email});
@@ -134,12 +134,12 @@ app.post('/api/grocerylist', (req, res) => {
 
 app.post('/api/combine', (req, res) => {
   const { ingredients, oldIngredients } = req.body;
-  res.send(combineIngredients(ingredients, oldIngredients));
+  res.send(helpers.combineIngredients(ingredients, oldIngredients));
 });
 
 app.post('/api/parse', (req, res) => {
   const { ingredients } = req.body;
-  res.send(parseIngredients(ingredients));
+  res.send(helpers.parseIngredients(ingredients));
 });
 
 app.post('/api/saverecipe', (req, res) => {
@@ -218,69 +218,19 @@ app.post('/api/signup', (req, res) => {
   });
 });
 
-//Helper Functions====================================================
-const units = {
-  teaspoon: 'tsp',
-  tablespoon: 'Tbs',
-  'fluid ounce': 'fl-oz',
-  cup: 'cup',
-  pint: 'pnt',
-  quart: 'qt',
-  gallon: 'gal',
-  ounce: 'oz',
-  pound: 'lb',
-  liter: 'l',
-};
-
-const unitsList = ['tsp', 'Tbs', 'fl-oz', 'cup', 'pnt', 'qt', 'gal', 'oz', 'lb', 'l'];
-
-// Takes in array of strings
-const parseIngredients = (ingredients) => {
-  let parsed = ingredients.map(ingredient => {
-    // Create object using 'recipe ingredient parser' module
-    let obj = parse(ingredient.toLowerCase());
-    // Convert to singular
-    obj.ingredient = pluralize.singular(obj.ingredient);
-    if (obj.unit) {
-      obj.unit = pluralize.singular(obj.unit);
-      // Convert to abbreviation
-      if (units[obj.unit]) {
-        obj.unit = units[obj.unit];
-      }
-    }
-    return obj;
+//Testing====================================================
+const tester = () => {
+  ingredients = dbHelpers.selectIngredients({email: 'theohzonelayer@gmail.com', table: 'ingredients'})
+  .then((results) => {
+    oldIngredients = results;
+    ingredients = [
+      {ingredient: 'sugar', quantity: 100, unit: 'oz'},
+      {ingredient: 'salt', quantity: 70, unit: 'oz'},
+    ];
+    console.log('COMBINE', helpers.combineIngredients(ingredients, oldIngredients));
   });
-  return parsed;
 }
-
-// Takes in two arrays of objects with quantity, unit, and ingredient properties
-const combineIngredients = (ingredients, oldIngredients) => {
-  // Converts the old ingredients array into an object
-  let ingredientsObj = {};
-  oldIngredients.forEach(ingredient => {
-    ingredientsObj[ingredient.ingredient] = { quantity: ingredient.quantity, unit: ingredient.unit }
-  });
-  // Compares elements from the new ingredients array to old ingredients and converts as necessary
-  let results = [];
-  ingredients.forEach(newIngredient => {
-    let old = ingredientsObj[newIngredient.ingredient];
-    if (old && unitsList.includes(old.unit) && unitsList.includes(newIngredient.unit)) {
-      newIngredient.quantity = convert(newIngredient.quantity).from(newIngredient.unit).to(old.unit);
-      newIngredient.unit = old.unit;
-      let combinedWithUnits = combine([newIngredient, { quantity: old.quantity, unit: old.unit, ingredient: newIngredient.ingredient }]);
-      results.push(combinedWithUnits[0]);
-    } else if (old && !old.unit && !newIngredient.unit) {
-      let combinedWithoutUnits = combine([newIngredient, { quantity: old.quantity, unit: null, ingredient: newIngredient.ingredient }]);
-      results.push(combinedWithoutUnits[0]);
-    } else {
-      results.push(newIngredient);
-    }
-  });
-  return results;
-};
-
-console.log(combineIngredients([{ingredient: 'apple', quantity: 2, unit: 'oz'}], 
-  [{ingredient: 'apple', quantity: 3, unit: 'lb'}]));
+// tester();
 //Listener====================================================
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${process.env.PORT || 3000}!`);
