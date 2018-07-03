09b027dcd420bceb3d488f79e181e35969d881eb
Calvin <calvinchui382025@yahoo.com>
2018-07-03 12:35:37 -0500

    merging

--- server/extCalls.js
+++ server/extCalls.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getRecipesByIngredients = (ingredients) => {
   // Turn passed in ingredients into query string
-  const url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?number=10&limitLicense=false&ranking=2&fillIngredients=true&ingredients=';
+  const url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?number=20&limitLicense=false&ranking=2&fillIngredients=true&ingredients=';
   let q = '';
   if (Array.isArray(ingredients)) {
     ingredients.forEach((ingredient, i) => {
@@ -16,8 +16,12 @@ const getRecipesByIngredients = (ingredients) => {
     q = ingredients;
   }
   return axios.get(url + q,
-    {'headers': {'X-Mashape-Key': process.env.SPOONACULAR_KEY,
-    'X-Mashape-Host': 'spoonacular-recipe-food-nutrition-v1.p.mashape.com'}})
+    {
+      'headers': {
+        'X-Mashape-Key': process.env.SPOONACULAR_KEY,
+        'X-Mashape-Host': 'spoonacular-recipe-food-nutrition-v1.p.mashape.com'
+      }
+    })
     .then((results) => {
       console.log('REQUESTS REMAINING:', results.headers['x-ratelimit-requests-remaining']);
       console.log('RESULTS REMAINING:', results.headers['x-ratelimit-results-remaining']);
@@ -25,16 +29,20 @@ const getRecipesByIngredients = (ingredients) => {
     }).catch((err) => {
       return err;
     });
-  
+
 }
 
 const getRecipeById = (id) => {
   const firstHalf = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/';
   const lastHalf = '/information?includeNutrition=true'
-  
+
   return axios.get(firstHalf + id + lastHalf,
-    {'headers': {'X-Mashape-Key': process.env.SPOONACULAR_KEY,
-    'X-Mashape-Host': 'spoonacular-recipe-food-nutrition-v1.p.mashape.com'}})
+    {
+      'headers': {
+        'X-Mashape-Key': process.env.SPOONACULAR_KEY,
+        'X-Mashape-Host': 'spoonacular-recipe-food-nutrition-v1.p.mashape.com'
+      }
+    })
     .then((results) => {
       console.log('REQUESTS REMAINING:', results.headers['x-ratelimit-requests-remaining']);
       console.log('RESULTS REMAINING:', results.headers['x-ratelimit-results-remaining']);
